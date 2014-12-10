82ed53161a20c616290462475b2fea5ac252090c
fmdm <fmdm@emedware.eu>
2014-12-10 13:05:45 +0200

    indeed

--- classy.js
+++ classy.js
@@ -7,20 +7,31 @@
  * - The code comes with no warranty of any kind: The author declines any responsability in anything, from data loss to kitty death
  */
 window.classy = (function() {
-	var classCounter = 0, rootObject = {}, objectCounter = 0, getSetCache = {}, constructorCalled = {};
+	function incString(s) {
+		//return 1+s;
+		if(''=== s) return String.fromCharCode(0);
+		var code = 1+ s.charCodeAt(0), substr = s.substr(1);
+		return 255< code?
+			String.fromCharCode(0)+incString(substr):
+			String.fromCharCode(code)+ substr;
+	}
+	var classCounter = '', objectCounter = '',
+	//var classCounter = 0, objectCounter = 0,
+		rootObject = {}, getSetCache = {}, constructorCalled = {};
 	Object.defineProperty(rootObject, 'legacy', {
 		enumerable: false,
 		get: function() {
-			var me= this, f = arguments.callee.caller, chain = f.caller.parent, called;
+			var me= this, f = arguments.callee.caller, chain = f.caller, called;
+			if(chain) chain = chain.parent;
 			if(chain) {
 				called = chain.bypass?chain.original||chain:chain;
-				return dob=ext(function() { return called.apply(me, arguments); }, {
+				return dob=ext(function ClassyLegacy() { return called.apply(me, arguments); }, {
 					parent: chain.parent,
-					chain: ext(function() {
+					chain: ext(function ClassyChain() {
 						var args = [].slice.call(arguments, 0);
 						return called.apply(me, args.concat([].slice.call(f.arguments, args.length)));
 					}, {parent: chain.parent}),
-					apply: ext(function(args) { return called.apply(me, args); }, {parent: chain.parent})
+					apply: ext(function ClassyApply(args) { return called.apply(me, args); }, {parent: chain.parent})
 				});
 			}
 			return classy.emptyLegacy;
@@ -31,18 +42,18 @@ window.classy = (function() {
 		var i, oFcts, mFcts;
 		function pairGetSet(name, originalValue, todos) {
 			var rv = {
-					set: todos.set&&todos.get?function(v) {
+					set: todos.set&&todos.get?function ClassyCacheSet(v) {
 						var uid= this.oid;
 						getSetCache[uid] || (getSetCache[uid]={});
 						getSetCache[uid][name] = v;
 					}:false,
 					get: todos.get?(
 						todos.set?
-						function() {
+						function ClassyCacheGet() {
 							var cached = getSetCache[this.oid];
 							return cached && cached.hasOwnProperty(name)?cached[name]:originalValue;
 						}:
-						function() { return originalValue; }
+						function ClassyCacheSet() { return originalValue; }
 					):false
 			};
 			if(rv.set) rv.set.terminal;
@@ -50,6 +61,7 @@ window.classy = (function() {
 			return rv;
 		}
 		function addLegacy(fct, last) {
+			if(!fct.original) return;	//This is the case for non-virtual functions
 			while(fct.parent) {
 				fct = fct.parent;
 				if(fct.isTerminal) return;
@@ -62,10 +74,15 @@ window.classy = (function() {
 		}
 		function cloneFct(original, name) {
 			if(!original) return;
-			var Classy;
+			var Classy, dbgcpt = 0;
 			Classy = 'constructor'=== name?
-				function() { ++constructorCalled[this.oid]; return original.apply(this, arguments); }:
-				ext(function() { return original.apply(this, arguments); }, {bypass: true});
+				function ClassyWrapper() { ++constructorCalled[this.oid]; return original.apply(this, arguments); }:
+				ext(function ClassyWrapper() {
+					if(100< ++dbgcpt)
+						debugger;
+					try{ return original.apply(this, arguments); }
+					finally {--dbgcpt;}
+				}, {bypass: true});
 			Object.defineProperty(Classy, 'original', {
 				value: original,
 				writable: false,
@@ -113,9 +130,10 @@ window.classy = (function() {
 
 //TODO: when .get returns a function and this function calls its legacy
 				if(obj.hasOwnProperty(i)) {
-					if(mFcts ^ ('function'=== typeof obj[i]))
-						throw new classy.exception('Member "'+i+'" is heritated - once a function once not');
-					if(mFcts) addLegacy(obj[i], oFcts);
+					if('function'=== typeof obj[i] && obj[i].original) {
+						if(mFcts) addLegacy(obj[i], oFcts);	//This keeps the virtual chain
+						else obj[i] = obj[i].original;	//This indicates it's not a virtual function but a data function (ie. a class specification, ...)
+					}	//If obj already has a non-virtual function or a non-functional data, just let it as it is
 				} else obj[i] = oFcts;
 			}
 		}
@@ -128,12 +146,14 @@ window.classy = (function() {
 					dst[j] = obj[j];
 		return dst;
 	}
-	ext(Function.prototype, { get terminal() { this.isTerminal = true; return this; } });
+	ext(Function.prototype, {
+		get terminal() { this.isTerminal = true; return this; },
+		classify: function RegularClassify(obj) { return obj instanceof this; }	// instanceof surrogate - in case of classy is changed to a regular class
+	});
 	
-	classy = ext(function(members) {
-		var rv = false,
-			xtnds = [].slice.call(arguments, 1), mFleg=[], orders = {}, i, j,
-			waitings = {}, ready = 'me', classes = {}, orgCtor, cname;
+	classy = ext(function Classy(members) {
+		var rv = false, xtnds = [].slice.call(arguments, 1), mFleg=[], orders = {},
+			i, j, waitings = {}, ready = '', classes = {}, orgCtor, cname, fname;
 		//rv -> {cidA: {cidB: true (A before B)} }
 		//classes -> {cid: class object}
 		function order(rv, classes, list, before, inheriting) {
@@ -141,15 +161,32 @@ window.classy = (function() {
 			function precede(a, b, force) {
 				rv[a] || (rv[a] = {});
 				rv[b] || (rv[b] = {});
-				if(!rv[b][a])
-					rv[a][b] = true;
-				else if(force) {
-					rv[a][b] = true;
-					delete rv[b][a];
+				
+				if(force) {
+					rv[a][b] = 'f';
+					//@ assert No contradictory force : 'f'!== rv[b][a]
+					var recur = 0;
+					function prune(nb) {
+						if(1000< ++recur)
+							debugger;
+						for(var i in orders[nb]) {
+							delete orders[i][a];
+							prune(i);
+						}
+						--recur;
+					}
+					prune(b);
+				} else if(!rv[a][b]) {
+					function allowed(nb) {
+						for(var i in orders[nb])
+							if(orders[i][a] || !allowed(i)) return false;
+						return true;
+					}
+					if(!rv[b][a] && allowed(b)) rv[a][b] = true;
 				}
 			}
 			for(i=0; i<list.length; ++i) {
-				clss = list[i];
+				if(!(clss = list[i])) throw new classy.exception('Empty inheritance');
 				if(classy!== clss.constructor) throw new classy.exception('Specified inheritance is not Classy');
 				classes[clss.cid] = clss;
 				for(j=0; j< inheriting.length; ++j) {
@@ -170,9 +207,9 @@ window.classy = (function() {
 			}
 		}
 		order(orders, classes, xtnds, [], [ready]);
-		for(i in orders.me) waitings[i] = 0;
+		for(i in orders['']) waitings[i] = 0;
 		for(i in orders) for(j in orders[i]) ++waitings[j];
-		while(ready) {
+		while(false!== ready) {
 			j = ready;
 			ready = false;
 			for(i in orders[j]) {
@@ -183,9 +220,12 @@ window.classy = (function() {
 			}
 			if(ready) mFleg.push(classes[ready]);
 		}
-		//todo: assert \/x, !waitings[x]
+		for(i in waitings)
+			//@ assert No class is left waiting in the 'to-extend' queue : !waitings[i]
+			;
 
 		members || (members = {});
+		//@ assert Given members are pure objects : Object.getPrototypeOf(members) === Object.prototype
 		if({}.constructor=== members.constructor) members.constructor = (function() {});
 		rv = Object.create(rootObject);
 		setMbrs(rv, members);
@@ -196,7 +236,7 @@ window.classy = (function() {
 			var me=this, ctr = orgCtor;
 			
 			Object.defineProperty(me, 'oid', {
-				value: ++objectCounter,
+				value: objectCounter = incString(objectCounter),
 				writable: false,
 				enumerable: false,
 				configurable: false
@@ -209,54 +249,83 @@ window.classy = (function() {
 			}
 			delete constructorCalled[me.oid];
 		};
-		cname = members.constructor.name || 'ClassyObject';
-		rv.constructor = eval('[function '+cname+'() { return ctor.apply(this, arguments); }]')[0];
+		if(members.constructor.name)
+			fname = cname = members.constructor.name;
+		else if(members.name) {
+			cname = members.name;
+			//'_' at the begining to avoid problems with figure-beginned words and reserved keywords
+			fname = '_'+cname.replace(/[^\w]/g, '_');
+		} else fname = cname = 'ClassyObject';
+		rv.constructor = eval('[function '+fname+'() { return ctor.apply(this, arguments); }]')[0];
 		Object.defineProperty(rv, 'constructor', {
 			enumerable: false,
 			writable: false,
 			configurable: false
 		});
-		return ext(
+		
+		ext(
 			rv.constructor,
-			{
+			classes = {
 				constructor: classy,
 				prototype: rv,
 				inherits: Object.freeze(xtnds),
 				members: Object.freeze(members),
 				fleg: Object.freeze(mFleg),
-				cid: ++classCounter,
-				classify: function(obj) {
+				cid: classCounter = incString(classCounter),
+				classify: function  Classiffy(obj) {
 					var me= this, ctr = obj.constructor;
-					return me === ctr || 0<= ctr.fleg.indexOf(me);
+					return me === ctr || (ctr.fleg && 0<= ctr.fleg.indexOf(me));
 				},
-				extends: function(parent) {
+				extends: function Extends(parent) {
 					return 0<= this.fleg.indexOf(parent);
 				},
-				toString: function() {
+				toString: function ToString() {
 					return '[class'+(cname?' '+cname:'')+']';
-				}
+				},
+				name: cname
 			}
 		);
+		for(i in classes) classes[i] = {
+			enumerable: false,
+			writable: false,
+			configurable: false
+		};
+		Object.defineProperties(rv, classes);
+		return rv.constructor;
 	}, {
 		emptyLegacy: ext(function() {}, {
 			chain: function() {},
 			apply: function() {}
 		}),
-		exception: function(msg) {
+		exception: ext(function ClassyException(msg) {
 			this.message = msg;
-		},
-		singleton: ext(function(members) {
+		}, {
+			prototype: {
+				toString: function() {
+					return 'ClassyError: '+this.message;
+				}
+			}
+		}),
+		singleton: ext(function ClassySingleton(members) {
 			var obj, xtnds = [].slice.call(arguments, 1), classes, mClass, i;
 			members || (members = {});
 			classes = [members];
 			for(i=0; i<xtnds.length; ++i)
-				classes.push(classy=== xtnds[i].constructor?xtnds[i]:xtnds[i].constructor);
+				if(!xtnds[i]) throw new classy.exception('Empty inheritance');
+				else classes.push(classy=== xtnds[i].constructor?xtnds[i]:xtnds[i].constructor);
+			classes.push(classy.singleton.root || (classy.singleton.root = classy({
+				constructor: function Singleton() {},
+				toString: function () {
+					var cname = this.constructor.name;
+					return '[singleton'+(cname?' '+cname:'')+']';
+				}
+			})));
 			mClass = classy.apply(this, classes);
 			fleg = mClass.fleg;
 			obj = new mClass();
 			return obj;
 		}, {
-			extendedMember: function(me, mbrName) {
+			extendedMember: function ClassyExtendedMember(me, mbrName) {
 				if(!(mbrName instanceof Array)) mbrName = [].slice.call(arguments, 1);
 				var i, j, mbr, fleg = me.constructor.fleg, rv;
 				for(i=0; i< mbrName.length; ++i) {
